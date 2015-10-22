4eccd36dbf63f4bcd52cb870137f7f36632d6a11
koseki <dainaso97@gmailcom>
2015-10-22 14:49:02 +0900

    キーの文字列をテーブルビューに表示

---
+++ JSONxUITableViewSample/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  JSONxUITableViewSample
+//
+//  Created by 前田 晃良 on 2015/10/22.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JSONxUITableViewSample/ViewController.swift
+++ JSONxUITableViewSample/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
 
+    var memberArray:NSMutableArray = []
+    var memberDictionary:NSDictionary = [:]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //プロジェクト内のJSONファイルを取得
+        var path = NSBundle.mainBundle().pathForResource("nintendoJSON", ofType: "json")
+        var jsonData = NSData(contentsOfFile:path!)
+        
+        memberDictionary = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: nil) as! NSDictionary
+        
+        for (key,dictionary) in memberDictionary{
+            memberArray.addObject(key) //キーを格納
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,17 +37,22 @@ class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSourc
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return memberArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:UITableViewCell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
-        cell.textLabel?.text = "\(indexPath.row)番目"
+        
+        var text = memberArray[indexPath.row] as! String
+        cell.textLabel?.text = text
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("\(indexPath.row)番目を選択")
+        
+        var memberName = memberArray[indexPath.row] as! String
+        println(memberDictionary[memberName])
     }
     
     override func prefersStatusBarHidden() -> Bool {
